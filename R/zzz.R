f8ce263277f4df6bfaf0d947d2365c739f4ac92a
ericdunipace <edunipace@g.harvard.edu>
2022-01-07 18:25:23 -0800

    citation comment

--- R/zzz.R
+++ R/zzz.R
@@ -8,5 +8,6 @@
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("For more information about how to use the header files, see the CGAL documentation  at <https://www.cgal.org>.\n
-Also, if using this package for commercial purposes, please see <https://www.cgal.org/license.html> for more details about licensing the CGAL header files.", domain = NULL, appendLF = TRUE)
+Also, if using this package for commercial purposes, please see <https://www.cgal.org/license.html> for more details about licensing the CGAL header files.\n
+Please cite this package if you use it. See citation('RcppCGAL').", domain = NULL, appendLF = TRUE)
 }
