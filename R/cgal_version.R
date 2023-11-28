c25ec921e83854cd5e620509eb3bfbaf0190648d
ericdunipace <edunipace@mail.harvard.edu>
2023-11-28 15:25:04 -0800

    cran dev hell

--- R/cgal_version.R
+++ R/cgal_version.R
@@ -6,7 +6,7 @@
 #' @examples 
 #' cgal_version()
 cgal_version <- function() {
-  message("Due to CRAN not responding to emails or perusing submission documents, whether you used your own CGAL or not is no longer recorded. This package is bundled with 5.6 but who knows what's gone on!?")
+  message("This package is bundled with 5.6 but CRAN would not allow this package to record whether you used your own CGAL or not.")
   # vers <- cgal_pkg_state$VERSION
   # if (is.na(vers)) stop("There has been an error where the version was not set. Please report this bug.")
   # message(vers)
