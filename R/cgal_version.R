ef2bb2ea4245d1293f7b99059b8bb356688b7db6
ericdunipace <edunipace@mail.harvard.edu>
2023-11-29 23:17:24 -0800

    update verbiage

--- R/cgal_version.R
+++ R/cgal_version.R
@@ -6,8 +6,7 @@
 #' @examples 
 #' cgal_version()
 cgal_version <- function() {
-  message("This package is bundled with 5.6 but CRAN would not allow this package to record whether you used your own CGAL or not.")
-  # vers <- cgal_pkg_state$VERSION
-  # if (is.na(vers)) stop("There has been an error where the version was not set. Please report this bug.")
-  # message(vers)
+  vers <- cgal_pkg_state$VERSION
+  if (is.na(vers)) stop("There has been an error where the version was not set. Please report this bug.")
+  message(vers)
 }
--- R/pkg_state.R
+++ R/pkg_state.R
@@ -1,5 +1,5 @@
 # environment to save whether header files have been cleaned
 # VERSION populated with version from package or user supplied (due to CRAN preference, this is not currently done)
 cgal_pkg_state <- list2env(list(
-  VERSION = 'This is CGAL version 5.6.0.',
+  VERSION = 'This package is bundled with CGAL version 5.6.',
   WARNED = FALSE))
