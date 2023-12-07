09be6e524ce6c538a75e4626fb5d141ed4e5990e
ericdunipace <edunipace@mail.harvard.edu>
2023-12-07 14:23:24 -0800

    remove examples per CRAN

--- R/RcppCGAL-package.R
+++ R/RcppCGAL-package.R
@@ -1,17 +1,5 @@
 #' @references 
 #' The CGAL Project. (2023). {CGAL} User and Reference Manual. Retrieved from <https://doc.cgal.org/latest/Manual/index.html>
-#' @examples
-#' \dontrun{
-#' # To use this in a C++ file make sure you add an appropriate
-#' # dependency in your header C++ code. Make sure to use CGAL/basic.h
-#' 
-#' #include <Rcpp.h>
-#' // [[Rcpp::depends(RcppCGAL)]]
-#' #include <CGAL/basic.h>
-#' 
-#' // function code
-#' 
-#' }
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
